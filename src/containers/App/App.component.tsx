3ac2b1b8ea05453d0a68244e8470bd889b0df51c
fjplaurr <fjplaurr@gmail.com>
2023-02-15 00:28:05 +0100

    feat: add first view

--- src/containers/App/App.component.tsx
+++ src/containers/App/App.component.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import styles from './App.module.scss';
+import Button from '../../components/Button';
 
-function App() {
-  return (
-    <div className={styles.container}>
-      <header>template-repository</header>
-      <h1 className={styles.title}>
-        This is a template for React in Typescript
-      </h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className={styles.container}>
+    <header>template-repository</header>
+    <h1 className={styles.title}>This is a template for React in Typescript</h1>
+    <Button text="Play" />
+  </div>
+);
 
 export default App;
