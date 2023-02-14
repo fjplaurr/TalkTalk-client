1ed8a78706d6276fd5ba57a28285ed7db17de431
fjplaurr <fjplaurr@gmail.com>
2023-02-14 23:15:51 +0100

    feat: add App container and set it at first view

---
+++ src/containers/App/App.component.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <header>template-repository</header>
+      <h1 className={styles.title}>
+        This is a template for React in Typescript
+      </h1>
+    </div>
+  );
+}
+
+export default App;
