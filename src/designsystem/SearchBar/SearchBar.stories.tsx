9ade3c968fb8dd9adb91df7835cc4b12d4e6dc7e
fjplaurr <fjplaurr@gmail.com>
2023-10-10 18:27:10 +0200

    fix: type asserion for function renderUser

--- src/designsystem/SearchBar/SearchBar.stories.tsx
+++ src/designsystem/SearchBar/SearchBar.stories.tsx
@@ -2,11 +2,7 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import SearchBar from './index';
-import Box from '../Box';
-import Text from '../Text';
-import Avatar from '../Avatar';
-import Button from '../Button';
-import styles from './SearchBar.module.css';
+import PostCard from '../../components/PostCard';
 
 const argTypes = {
   onInputChange: { action: 'typed' },
@@ -22,68 +18,47 @@ const Template: ComponentStory<typeof SearchBar> = (args) => (
   <SearchBar {...args} />
 );
 
+type User = {
+  pictureSrc: string;
+  name: string;
+  surname: string;
+  text: string;
+  id: string;
+};
+
+type ObjectWithId = { [key: string]: any; id: string };
+
+const renderUser = (user: User) => <PostCard user={user} />;
+
+const users = [
+  {
+    id: '1',
+    isFollowed: true,
+    name: 'John',
+    surname: 'Doe',
+    pictureSrc: 'https://placedog.net/600',
+    text: 'Loren impsum dolor sit amet',
+  },
+  {
+    id: '5',
+    isFollowed: false,
+    name: 'Peter',
+    surname: 'Jackson',
+    pictureSrc: 'https://placedog.net/330',
+    text: 'Loren impsum',
+  },
+  {
+    id: '15',
+    isFollowed: false,
+    name: 'Andrew',
+    surname: 'Mckey',
+    pictureSrc: 'https://placedog.net/130',
+    text: 'Loren impsum dolor sit Loren impsum dolor sit amet amet amet',
+  },
+];
+
 export const Default = Template.bind({});
 Default.args = {
-  elements: [
-    {
-      id: '1',
-      isFollowed: true,
-      name: 'John',
-      surname: 'Doe',
-      pictureSrc: 'https://placedog.net/600',
-      text: 'Loren impsum dolor sit amet',
-    },
-    {
-      id: '5',
-      isFollowed: false,
-      name: 'Peter',
-      surname: 'Jackson',
-      pictureSrc: 'https://placedog.net/330',
-      text: 'Loren impsum',
-    },
-    {
-      id: '15',
-      isFollowed: false,
-      name: 'Andrew',
-      surname: 'Mckey',
-      pictureSrc: 'https://placedog.net/130',
-      text: 'Loren impsum dolor sit Loren impsum dolor sit amet amet amet',
-    },
-  ],
-  renderElement: (element) => (
-    <Box
-      display="flex"
-      alignItems="flex-start"
-      noBorder
-      pt="12px"
-      pr="12px"
-      pb="12px"
-      pl="12px"
-      width="100%"
-      gap="16px"
-    >
-      <Avatar avatar={{ src: element.pictureSrc, name: element.name }} />
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="flex-start"
-        flexDirection="column"
-        noBorder
-      >
-        <Text fontWeight="bold">{`${element.name} ${element.surname}`}</Text>
-        <Text fontWeight="regular" fontSize="small">
-          {element.text}
-        </Text>
-      </Box>
-      <div className={styles['button-wrapper']}>
-        <Button
-          width="98px"
-          variant={element.isFollowed ? 'destructive' : 'secondary'}
-          onClick={() => console.log(`Clicked row`)}
-        >
-          {element.isFollowed ? 'Unfollow' : 'Follow'}
-        </Button>
-      </div>
-    </Box>
-  ),
+  elements: users,
+  renderElement: renderUser as (obj: ObjectWithId) => JSX.Element,
 };
