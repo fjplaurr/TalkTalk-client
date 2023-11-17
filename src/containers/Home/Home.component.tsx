3c9adb90c1a34ebc19ad7d03646635f80c64a55e
fjplaurr <fjplaurr@gmail.com>
2023-11-17 14:51:15 +0100

    feat(Home page): create UX

--- src/containers/Home/Home.component.tsx
+++ src/containers/Home/Home.component.tsx
@@ -1,55 +1,91 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 import {
   Box,
   Theme,
   Logo,
-  Icons,
   TextArea,
   SearchBar,
   PostCard,
+  ProfileCard,
+  TabProvider,
 } from '../../designsystem';
-import { User } from '../../interfaces';
+import { User, Post } from '../../interfaces';
 import Card from './components/Card';
+import EditProfile from './components/EditProfile';
 
-const users = [
+const usersFromDB: User[] = [
   {
     id: '1',
-    isFollowed: true,
-    name: 'John',
-    surname: 'Doe',
+    firstName: 'John',
+    lastName: 'Doe',
     pictureSrc: 'https://placedog.net/600',
-    text: 'Loren impsum dolor sit amet',
+    email: 'todo',
+    password: 'todo',
+    followingUsers: [],
+    status: 'loren ipsum dolor sit amet',
   },
   {
-    id: '5',
-    isFollowed: false,
-    name: 'Peter',
-    surname: 'Jackson',
-    pictureSrc: 'https://placedog.net/330',
-    text: 'Loren impsum',
-  },
-  {
-    id: '15',
-    isFollowed: false,
-    name: 'Andrew',
-    surname: 'Mckey',
-    pictureSrc: 'https://placedog.net/130',
-    text: 'Loren impsum dolor sit Loren impsum dolor sit amet amet amet',
+    id: '2',
+    firstName: 'Steve',
+    lastName: 'Jones',
+    pictureSrc: 'https://placedog.net/700',
+    email: 'todo',
+    password: 'todo',
+    followingUsers: [],
+    status: 'loren ipsum dolor sit amet con etiam',
   },
 ];
 
-const postCardUser = {
-  name: 'John',
-  surname: 'Doe',
-  pictureSrc: 'https://placedog.net/420',
-  text: 'Adipisicing nulla labore mollit cillum ullamco dolor amet consectetur aliquip aliqua in in exercitation deserunt. Ex anim laboris esse sint ipsum ad. Nulla pariatur eu deserunt cillum incididunt mollit amet Lorem. Qui sint do laboris in ex magna laborum. Eu sint velit non qui id fugiat qui pariatur cillum anim veniam laboris id. Consequat culpa nostrud irure ad sunt ipsum minim aliqua proident non consectetur tempor voluptate.',
-  id: '1',
+const postsFromDB: Post[] = [
+  {
+    id: '1',
+    text: 'Loren impsum isicing nulla labore mollit cillum ullamco dolor amet consectetur aliquip ali dolor sit amet',
+    date: new Date(),
+    authorId: '1',
+  },
+  {
+    id: '2',
+    text: 'Loren impsum isicing nuonsectetur aliquip ali dolor sit amet',
+    date: new Date(),
+    authorId: '2',
+  },
+];
+
+const postsWithAuthor = postsFromDB.map((post) => {
+  const author = usersFromDB.find((user) => user.id === post.authorId)!;
+  return {
+    post: { description: post.text, id: post.id },
+    user: {
+      name: author.firstName!,
+      surname: author.lastName!,
+      pictureSrc: author.pictureSrc!,
+    },
+  };
+});
+
+type UserForSearchBar = {
+  id: string;
+  isFollowed: boolean;
+  name: string;
+  surname: string;
+  pictureSrc: string;
+  text: string;
 };
 
-type ObjectWithId = { [key: string]: any; id: string };
+const usersForSearchBar: UserForSearchBar[] = usersFromDB.map((user) => ({
+  id: user.id,
+  isFollowed: true,
+  name: user.firstName,
+  surname: user.lastName,
+  pictureSrc: user.pictureSrc,
+  text: user.status,
+}));
 
-const renderUser = (user: User) => <PostCard user={user} />;
+const searchBarRenderElement = (user: UserForSearchBar) => (
+  <ProfileCard user={user} />
+);
 
 const PageContainer = styled(Box)`
   padding-inline: 4vw;
@@ -59,43 +95,106 @@ const PageContainer = styled(Box)`
   }
 `;
 
-const Home = () => (
-  <PageContainer
-    $mt={Theme.setSpace(16)}
-    $display="flex"
-    $flexDirection="column"
-    $gap={Theme.setSpace(24)}
-  >
-    <Box
+type UserForProfileCard = {
+  isFollowed: boolean;
+  name: string;
+  surname: string;
+  pictureSrc: string;
+  text: string;
+  id: string;
+};
+
+const usersForProfileCard: UserForProfileCard[] = usersFromDB.map((user) => ({
+  isFollowed: true,
+  id: user.id,
+  name: user.firstName,
+  pictureSrc: user.pictureSrc,
+  surname: user.lastName,
+  text: user.status,
+}));
+
+type HomeProps = { isLoggedIn?: boolean };
+
+const Home = ({ isLoggedIn = true }: HomeProps) => {
+  const navigate = useNavigate();
+
+  const userNotLoggedInRedirect = () => {
+    console.log('isLoggedIn', isLoggedIn);
+    if (!isLoggedIn) {
+      navigate('/login');
+    }
+  };
+
+  return (
+    <PageContainer
+      $mt={Theme.setSpace(16)}
       $display="flex"
-      $flexDirection="row"
+      $flexDirection="column"
       $gap={Theme.setSpace(24)}
-      $justifyContent="center"
     >
-      <Logo color="darkBlue" />
-      <Box $flexGrow={1}>
-        <SearchBar
-          elements={users}
-          renderElement={renderUser as (obj: ObjectWithId) => JSX.Element}
-          onInputChange={() => console.log('typed')}
+      <Box
+        $display="flex"
+        $flexDirection="row"
+        $gap={Theme.setSpace(24)}
+        $justifyContent="center"
+      >
+        <Logo color="darkBlue" />
+
+        <Box $flexGrow={1} onClick={userNotLoggedInRedirect}>
+          <SearchBar
+            elements={usersForSearchBar}
+            renderElement={searchBarRenderElement}
+            onInputChange={() => console.log('typed')}
+          />
+        </Box>
+        <EditProfile />
+      </Box>
+
+      <Box onClick={userNotLoggedInRedirect}>
+        <TextArea
+          onChange={(event) => console.log(event.target.value)}
+          placeholder={
+            isLoggedIn
+              ? 'Do you want to share something?'
+              : 'Click Login and start sending messages...'
+          }
+          $resize="vertical"
+          onSend={() => console.log('a')}
+          buttonText={isLoggedIn ? 'Send' : 'Login'}
         />
       </Box>
-      <Icons.UserIcon />
-    </Box>
-
-    <TextArea
-      onChange={(event) => console.log(event.target.value)}
-      placeholder="Click Login and start sending messages..."
-      resize="vertical"
-    />
-    <Card>
-      <PostCard user={postCardUser} />
-      <PostCard user={postCardUser} />
-      <PostCard user={postCardUser} />
-      <PostCard user={postCardUser} />
-      <PostCard user={postCardUser} />
-    </Card>
-  </PageContainer>
-);
+      <Box onClick={userNotLoggedInRedirect}>
+        <Card>
+          <TabProvider
+            tabs={[
+              {
+                id: 'For you',
+                title: 'For you',
+                content: (
+                  <>
+                    {postsWithAuthor.map(({ user, post }) => (
+                      <PostCard user={user} post={post} key={post.id} />
+                    ))}
+                  </>
+                ),
+              },
+              {
+                id: 'Following',
+                title: 'Following',
+                content: (
+                  <>
+                    {usersForProfileCard.map((user) => (
+                      <ProfileCard user={user} key={user.id} />
+                    ))}
+                  </>
+                ),
+              },
+            ]}
+          />
+        </Card>
+      </Box>
+    </PageContainer>
+  );
+};
 
 export default Home;
